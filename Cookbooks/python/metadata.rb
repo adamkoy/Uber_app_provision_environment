8f4572ad0c8de7eee395d07237e53130def99c7b
Adam Koyuncu <AKoyuncu@spartaglobal.com>
2019-08-19 11:42:31 +0100

    Created the recipe for python plugs and started unit tests

--- Cookbooks/python/metadata.rb
+++ Cookbooks/python/metadata.rb
@@ -8,8 +8,6 @@ version '0.1.0'
 chef_version '>= 14.0'
 
 
-depends 'python', '~> 2.5.2'
-depends 'pytest', '~>1.6'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- Cookbooks/python/spec/unit/recipes/default_spec.rb
+++ Cookbooks/python/spec/unit/recipes/default_spec.rb
@@ -15,15 +15,101 @@ describe 'python::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs the package python' do
+      expect(chef_run).to install_package('python')
     end
+
+
+    it 'installs the package python' do
+      expect(chef_run).to install_package('python-pip')
+    end
+
+    it 'installs the package python' do
+      expect(chef_run).to install_package('libncurses5-dev')
+    end
+
+    it 'installs the dependency for gnureadline' do
+      expect(chef_run).to install_package('libffi-dev')
+    end
+
+    it 'installs the dependency for gnureadline' do
+      expect(chef_run).to install_service('Flask').with (version '0.10.1')
+    end
+
+    #
+    #
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install Flask==0.10.1'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install Jinja2==2.7.3'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install MarkupSafe==0.23'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install Werkzeug==0.9.6'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install gnureadline==6.3.3'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install itsdangerous==0.24'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install rauth==0.7.0'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install requests==2.3.0'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install gunicorn==18.0'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install Flask-SSLify==0.1.4'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install pytest==2.5.2'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install pytest-cov==1.6'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install betamax==0.4.0'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install flake8==2.1.0'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install pep8==1.5.6'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install pyflakes==0.8.1'
+    # end
+    #
+    # execute 'python-pip install plugins' do
+    #   command 'pip install coveralls==0.4.2'
+    # end
+    #
+    #
+    #
+
+
   end
 end
