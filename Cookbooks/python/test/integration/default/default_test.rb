a21b8abe3d81eaf3b6b636d7dddc474d417fd22a
Adam Koyuncu <AKoyuncu@spartaglobal.com>
2019-08-19 14:37:29 +0100

    successfully created integration tests for python packages

--- Cookbooks/python/test/integration/default/default_test.rb
+++ Cookbooks/python/test/integration/default/default_test.rb
@@ -20,6 +20,15 @@ describe package('python') do
   it { should be_installed }
 end
 
+describe package('python-pip') do
+  it { should be_installed }
+end
+describe package('libncurses5-dev') do
+  it { should be_installed }
+end
+describe package('libffi-dev') do
+  it { should be_installed }
+end
 
 describe pip('Flask') do
   it { should be_installed }
